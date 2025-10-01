53a07f04018572c1034161ded6579f59ffdc5675
amastaneh@gmail.com <amastaneh@gmail.com>
2025-09-30 20:45:55 -0400

    Adding deploy

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'geo-business-search'
+
+module.exports = {
+    output: 'export',
+    images: { unoptimized: true },
+    basePath: isProd ? `/${repo}` : '',
+    assetPrefix: isProd ? `/${repo}/` : '',
+}
